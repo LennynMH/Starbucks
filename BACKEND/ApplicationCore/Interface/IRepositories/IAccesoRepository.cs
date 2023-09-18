338ddc9d0dd1d0873d079c020c4cd30ac17d4ed9
Lennyn MH <lennyn_josue@hotmail.com>
2023-09-17 20:02:08 -0500

    Initial commit

--- BACKEND/ApplicationCore/Interface/IRepositories/IAccesoRepository.cs
+++ BACKEND/ApplicationCore/Interface/IRepositories/IAccesoRepository.cs
@@ -4,6 +4,6 @@ namespace ApplicationCore.Interface.IRepositories
 {
     public interface IAccesoRepository
     {
-        Task<UsuarioEntity?> ValidaAccesoUsuario(string? Codigo, string? Contrasena);
+        Task<UsuarioEntity?> ValidaAccesoUsuario(string Codigo, string Contrasena);
     }
 }
